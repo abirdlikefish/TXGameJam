e21c5b30226fc377e7fe096efda57c7157595e83
DeliGitee <1765939870@qq.com>
2024-10-23 06:20:17 +0800

    10.23.6
    玩家2d贴图测试

--- Assets/Scripts/Material/Flash.cs
+++ Assets/Scripts/Material/Flash.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Flash : NewMaterial
@@ -7,16 +8,31 @@ public class Flash : NewMaterial
     public float a;
     public float b;
     public float div = 5;
+    Color c;
     void Update()
-    {
+    { 
         //让c的a值在a,b之间变化
-        Color c = Material.GetColor("_Diffuse");
+        if (meshRenderer == null)
+        {
+            c = spriteRenderer.color;
+            c.a = Mathf.PingPong(Time.time / div, b - a) + a;
+            spriteRenderer.color = c;
+            return;
+        }
+        c = Material.GetColor("_Diffuse");
         c.a = Mathf.PingPong(Time.time / div, b - a) + a;
         Material.SetColor("_Diffuse", c);
     }
     private void OnDisable()
     {
-        Color c = Material.GetColor("_Diffuse");
+        if (meshRenderer == null)
+        {
+            c = spriteRenderer.color;
+            c.a = 1;
+            spriteRenderer.color = c;
+            return;
+        }
+        c = Material.GetColor("_Diffuse");
         c.a = 1;
         Material.SetColor("_Diffuse", c);
     }
--- Assets/Scripts/Material/NewMaterial.cs
+++ Assets/Scripts/Material/NewMaterial.cs
@@ -6,16 +6,28 @@ public class NewMaterial : MonoBehaviour
 {
     public MeshRenderer meshRenderer;
     public int materialId;
+    public SpriteRenderer spriteRenderer;
     public Material Material
     {
-        get => meshRenderer.materials[materialId];
-        set => meshRenderer.materials[materialId] = value;
+        get => meshRenderer == null ? spriteRenderer.material : meshRenderer.materials[materialId];
+        set
+        {
+            if(meshRenderer == null)
+            {
+                spriteRenderer.material = value;
+            }
+            else
+            {
+                meshRenderer.materials[materialId] = value;
+            }
+        }
     }
     private void Awake()
     {
+        
         //生成材质实例
         Material material = Instantiate(Material);
         //赋值给meshRenderer
-        meshRenderer.materials[materialId] = material;
+        Material = material;
     }
 }
