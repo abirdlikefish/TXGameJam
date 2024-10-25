39d5852b85f6235e83f4cb602f6cf9029091e7f4
DeliGitee <1765939870@qq.com>
2024-10-25 17:42:42 +0800

    17 41

--- Assets/Scripts/Material/DepthSetterEntity.cs
+++ Assets/Scripts/Material/DepthSetterEntity.cs
@@ -6,41 +6,32 @@ public class DepthSetterEntity : MonoBehaviour
 {
     Vector3 ThisCenter => transform.position;
     public int d1;
-    private void Start()
+    void Update()
     {
-        StartCoroutine(SetDepth());
-    }
-    IEnumerator SetDepth()
-    {
-        while (true)
+        int isPassable = EventManager.Instance.IsPassable(MateInput.MyWorldToScreen(ThisCenter));
+        int isEmpty = EventManager.Instance.IsEmpty(MateInput.MyWorldToScreen(ThisCenter));
+        if ((isPassable == 0
+            ||
+            isEmpty != 0) 
+            && !GetComponent<HammerEffect>()
+            )
         {
-            int isPassable = EventManager.Instance.IsPassable(MateInput.MyWorldToScreen(ThisCenter));
-            int isEmpty = EventManager.Instance.IsEmpty(MateInput.MyWorldToScreen(ThisCenter));
-            if ((isPassable == 0
-                ||
-                isEmpty != 0) 
-                && !GetComponent<HammerEffect>()
-                )
-            {
-                Debug.Log($"destroy {ThisCenter} {name} pass{isPassable} mt{isEmpty}");
-                Destroy(gameObject);
-            }
-            int h1 = GetLeftCubeD(ThisCenter);
-            if (h1 == int.MinValue)
-            {
-                yield return 0;
-                continue;
-            }
-            int h2 = GetRightCubeD(ThisCenter);
-            if (h2 == int.MinValue)
-            {
-                yield return 0;
-                continue;
-            }
-            d1 = 3000 + h1 + h2 + 2;
-            GetComponent<NewMaterial>().Material.renderQueue = d1;
-            yield return 0;
+            Debug.Log($"destroy {ThisCenter} {name} pass{isPassable} mt{isEmpty}");
+            Destroy(gameObject);
         }
+        int h1 = GetLeftCubeD(ThisCenter);
+        if (h1 == int.MinValue)
+        {
+        return;
+        }
+        int h2 = GetRightCubeD(ThisCenter);
+        if (h2 == int.MinValue)
+        {
+            return;
+        }
+        d1 = 3000 + h1 + h2 + 2;
+        GetComponent<NewMaterial>().Material.renderQueue = d1;
+        
     }
 
     int GetLeftCubeD(Vector3 center)
