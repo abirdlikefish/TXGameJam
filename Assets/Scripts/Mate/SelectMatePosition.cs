1e5f7e35022e20ec244f6b05776c9803ac62b7dd
abirdlikefish <abirdlikefish@qq.com>
2024-10-25 11:27:27 +0800

    11:27

--- Assets/Scripts/Mate/SelectMatePosition.cs
+++ Assets/Scripts/Mate/SelectMatePosition.cs
@@ -25,6 +25,7 @@ public class SelectMatePosition : MonoBehaviour
 
                 instance.redColor = new Color(1, 0, 0, 0.5f);
                 instance.greenColor = new Color(0, 1, 0, 0.5f);
+                instance.selecetedGreenColor = new Color(0, 1, 0, 1);
                 // instance.mate0.transform.position = new Vector3(instance.position0.x, 0, instance.position0.y);
                 // instance.mate1.transform.position = new Vector3(instance.position1.x, 0, instance.position1.y);
             }
@@ -47,6 +48,7 @@ public class SelectMatePosition : MonoBehaviour
     // Color greenColor1;
     Color redColor;
     Color greenColor;
+    Color selecetedGreenColor;
 
     bool isReady0;
     bool isReady1;
@@ -106,7 +108,7 @@ public class SelectMatePosition : MonoBehaviour
             if(Input.GetKeyDown(KeyCode.Space)&& EventManager.Instance.IsPassable(CameraManager.Instance.GetCameraSpacePosition(new Vector3Int(Position0.x, 0, Position0.y))) == 3)
             {
                 isReady0 = true;
-                meshRenderer0.material.color = greenColor;
+                meshRenderer0.material.color = selecetedGreenColor;
             }
         }
         if(isReady1 == false)
@@ -159,10 +161,10 @@ public class SelectMatePosition : MonoBehaviour
                     meshRenderer1.material.color = redColor;
                 }
             }
-            if(Input.GetKeyDown(KeyCode.Return) && EventManager.Instance.IsPassable(CameraManager.Instance.GetCameraSpacePosition(new Vector3Int(Position1.x, 0, Position1.y))) == 3)
+            if(Input.GetKeyDown(KeyCode.Return )||Input.GetKeyDown(KeyCode.KeypadEnter) && EventManager.Instance.IsPassable(CameraManager.Instance.GetCameraSpacePosition(new Vector3Int(Position1.x, 0, Position1.y))) == 3)
             {
                 isReady1 = true;
-                meshRenderer1.material.color = greenColor;
+                meshRenderer1.material.color = selecetedGreenColor;
             }
         }
         if(isReady0 && isReady1)
@@ -182,8 +184,24 @@ public class SelectMatePosition : MonoBehaviour
         isReady1 = false;
         Position0 = Vector2Int.right;
         Position1 = Vector2Int.left;
-        meshRenderer0.material.color = redColor;
-        meshRenderer1.material.color = redColor;
+        if(EventManager.Instance.IsPassable(CameraManager.Instance.GetCameraSpacePosition(new Vector3Int(Position0.x, 0, Position0.y))) == 3)
+        {
+            meshRenderer0.material.color = greenColor;
+        }
+        else
+        {
+            meshRenderer0.material.color = redColor;
+        }
+        if(EventManager.Instance.IsPassable(CameraManager.Instance.GetCameraSpacePosition(new Vector3Int(Position1.x, 0, Position1.y))) == 3)
+        {
+            meshRenderer1.material.color = greenColor;
+        }
+        else
+        {
+            meshRenderer1.material.color = redColor;
+        }
+        // meshRenderer0.material.color = redColor;
+        // meshRenderer1.material.color = redColor;
         mate0.SetActive(true);
         mate1.SetActive(true);
         gameObject.SetActive(true);
